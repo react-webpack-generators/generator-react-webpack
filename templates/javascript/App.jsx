191f3c8b3fb61a7d66feff7730f50ce7e0d2dd19
Ryan Albrecht <ryan@ryanalbrecht.ca>
2014-07-05 23:42:09 -0400

    Export `window.React` so that the react-devtools can find it.
    The chrome tools first looks to see if `React` is defined globally, then if it's available via a call to `require()`. See https://github.com/facebook/react-devtools/blob/master/injected/ReactHost.js#L42-L48

--- templates/javascript/App.jsx
+++ templates/javascript/App.jsx
@@ -7,6 +7,9 @@
 var React = require('react/addons');
 var ReactTransitionGroup = React.addons.TransitionGroup;
 
+// Export React so the devtools can find it
+(window !== window.top ? window.top : window).React = React;
+
 // CSS
 require('../../styles/reset.css');
 require('../../styles/main.css');
