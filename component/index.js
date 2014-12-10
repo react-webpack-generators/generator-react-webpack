505d7277e9f3c2206927c6b05c42e2ead94da9e4
Emil Stenberg <esandin@gmail.com>
2014-12-10 12:45:17 +0100

    generating es6 modules if --es6 flag is used

--- component/index.js
+++ component/index.js
@@ -3,12 +3,19 @@ var util = require('util');
 var ScriptBase = require('../script-base.js');
 
 var ComponentGenerator = module.exports = function ComponentGenerator(args, options, config) {
+
   ScriptBase.apply(this, arguments);
 };
 
 util.inherits(ComponentGenerator, ScriptBase);
 
 ComponentGenerator.prototype.createComponentFile = function createComponentFile() {
+    this.option('es6');
+
+
+  this.es6 = this.options.es6;
+    console.log('es6:', this.es6)
+
   this.generateSourceAndTest(
     'Component',
     'spec/Component',
