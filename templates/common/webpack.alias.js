6ed2b8c484fc37b0fb1e9c79861cf5b5860e686e
yonatanmn <yonatanmn@users.noreply.github.com>
2015-08-02 14:15:02 +0300

    Create webpack.alias.js

---
+++ templates/common/webpack.alias.js
@@ -0,0 +1,7 @@
+ alias: {
+      'styles': __dirname + '/src/styles',
+      'mixins': __dirname + '/src/mixins',
+      'components': __dirname + '/src/components/'<% if(architecture==='flux'||architecture=='reflux') { %>,
+      'stores': __dirname + '/src/stores/',
+      'actions': __dirname + '/src/actions/'<% } %>
+    }
