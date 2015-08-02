1f10ac394cc8074c967bfdf817bd69f142ff7596
yonatanmn <yonatanmn@users.noreply.github.com>
2015-08-02 14:22:54 +0300

    update webpack.dist.config to have alias variable

--- templates/common/_webpack.dist.config.js
+++ templates/common/_webpack.dist.config.js
@@ -7,6 +7,7 @@
 'use strict';
 
 var webpack = require('webpack');
+var alias = require('./webpack.alias');
 
 module.exports = {
 
@@ -34,13 +35,7 @@ module.exports = {
 
   resolve: {
     extensions: ['', '.js'],
-    alias: {
-      'styles': __dirname + '/src/styles',
-      'mixins': __dirname + '/src/mixins',
-      'components': __dirname + '/src/components/'<% if(architecture==='flux'||architecture=='reflux') { %>,
-      'stores': __dirname + '/src/stores/',
-      'actions': __dirname + '/src/actions/'<% } %>
-    }
+    alias: alias
   },
 
   module: {
